8b47e6943d5f4f3cf19d1255384febdb46211ff8
Vogel <aed23nnu@uea.ac.uk>
2025-11-12 17:20:07 +0000

    starting work on java classes

--- Card.java
+++ Card.java
@@ -1,12 +1,18 @@
 public class Card {
 
-    public Bool face_up;
-    public int value = 0;
-    public String face;
+    private Bool face_up;
+    private int value = 0;
+    private String face;
 
     public Card (Bool face_up, int value, String face){
         this.face_up = face_up;
         this.value = value;
         this.face = face;
+    }
+
+    public int getValue(){return this.value;}
+
+    public void turnFaceUp(){
+        face_up = True;
     }
 }
