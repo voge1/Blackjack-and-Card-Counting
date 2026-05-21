0ceb1efb2a032fae408cbf663a9786c04a42ac12
Vogel <aed23nnu@uea.ac.uk>
2026-05-21 22:17:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,11 +9,11 @@ public class Main {
         ArrayList<Card> player_3_hand = new ArrayList<Card>();
         ArrayList<Card> dealer_hand = new ArrayList<Card>();
 
-        Shoe shoe = new Shoe(order, 4);
-        Player player_1 = new Player("John", player_1_hand, 100);
-        Player player_2 = new Player("Jane", player_2_hand, 100);
-        Player player_3 = new Player("Jax", player_3_hand, 100);
-        Dealer dealer = new Dealer("Dealer", dealer_hand, 10000);
+        Shoe shoe = new Shoe(order, 6);
+        Player player_1 = new Player("Dealer Mimic", player_1_hand, 1000);
+        Player player_2 = new Player("Basic Strategy", player_2_hand, 1000);
+        Player player_3 = new Player("Hi-Lo Counting", player_3_hand, 1000);
+        Dealer dealer = new Dealer("Dealer", dealer_hand, 100000);
         ArrayList<Player> players = new ArrayList<Player>();
         players.add(player_1); players.add(player_2); players.add(player_3);
         Blackjack game = new Blackjack(players, dealer, shoe, 3);
